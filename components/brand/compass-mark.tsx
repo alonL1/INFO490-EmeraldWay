711e110773ba91317f2fe765613617216a294ebc
alonl1 <alonlevy04@gmail.com>
2026-05-13 15:27:47 -0700

    logo fix

--- components/brand/compass-mark.tsx
+++ components/brand/compass-mark.tsx
@@ -9,10 +9,10 @@ type CompassMarkProps = {
 export function CompassMark({ className, priority = false }: CompassMarkProps) {
   return (
     <Image
-      src="/brand/compass-mark.svg"
+      src="/brand/commcompass_logo.png"
       alt="Community Compass logo mark"
-      width={88}
-      height={113}
+      width={346}
+      height={447}
       priority={priority}
       className={cn("h-auto w-[clamp(2.5rem,7vw,3.5rem)]", className)}
     />
